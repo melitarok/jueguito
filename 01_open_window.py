72dc6932141d38bbd99440f9f96dd616ca882dfa
Eva <evamacielderito@gmail.com>
2022-04-13 12:57:36 -0300

    cambio de tamaño, color y nombre

--- 01_open_window.py
+++ 01_open_window.py
@@ -4,9 +4,9 @@ juego de plataforma
 import arcade
 
 # Constantes
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 650
-SCREEN_TITLE = 'plataforma'
+SCREEN_WIDTH = 1500
+SCREEN_HEIGHT = 500
+SCREEN_TITLE = 'Juego De Plataforma'
 
 
 class MyGame(arcade.Window):
@@ -19,10 +19,10 @@ class MyGame(arcade.Window):
         # Llamar a la clase superior y programar la ventana
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
         
-        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+        arcade.set_background_color(arcade.csscolor.PLUM)
         
     def setup(self):
-        """Programar el juego aqui. Llama esta funcion para reiniciar el uego"""
+        """Programar el juego aqui. Llama esta funcion para reiniciar el juego"""
         pass
     
     def on_draw(self):
